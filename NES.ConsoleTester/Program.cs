a9ed5e81868343781756d6bf158eac9c50e8be4e
Jakob Farian Krarup <kr@rup.dk>
2023-03-22 22:07:31 +0100

    Added simple console text game for testing.

---
+++ NES.ConsoleTester/Program.cs
@@ -0,0 +1,130 @@
+﻿using NESControllerLibrary;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NES.ConsoleTester
+{
+    public class Program
+    {
+        private static NESController controller = new NESController();
+        private static Random random = new Random();
+        private static Point mapSize = new Point(15, 10), playerLocation = GetRandomPointOnBoard(), food = GetRandomPointOnBoard();
+        private static object lockObject = new();
+        private static Timer timer;
+        private static char playerChar = 'O';
+
+        static void Main(string[] args)
+        {
+
+            TimerCallback callBack = TimerCallBackMethod;
+            timer = new Timer(callBack, null, 0, 250);
+
+            Console.SetWindowSize(mapSize.X, mapSize.Y);
+            Console.SetBufferSize(mapSize.X, mapSize.Y);
+            Console.SetWindowSize(mapSize.X, mapSize.Y);
+            Console.CursorVisible = false;
+            controller.ButtonStateChanged += Controller_ButtonStateChanged;
+
+            while (playerLocation == food)
+            {
+                playerLocation = GetRandomPointOnBoard();
+            }
+            DrawScreen();
+            Console.ReadLine();
+        }
+
+        private static void DrawScreen()
+        {
+            lock (lockObject)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                for (int y = 0; y < mapSize.Y; y++)
+                {
+                    for (int x = 0; x < mapSize.X; x++)
+                    {
+                        if (y == 0 || x == 0 || y == mapSize.Y - 1 || x == mapSize.X - 1)
+                        {
+                            Console.SetCursorPosition(x, y);
+                            Console.Write("*");
+                        }
+                    }
+                }
+
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                for (int y = 1; y < mapSize.Y - 1; y++)
+                {
+                    for (int x = 1; x < mapSize.X - 1; x++)
+                    {
+                        Console.SetCursorPosition(x, y);
+                        Console.Write(".");
+                    }
+                }
+
+                Console.SetCursorPosition(playerLocation.X, playerLocation.Y);
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write(playerChar);
+
+                Console.SetCursorPosition(food.X, food.Y);
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write("%");
+                Console.ResetColor();
+            }
+        }
+
+        private static void Controller_ButtonStateChanged(object? sender, NESControllerEventArgs e)
+        {
+            if (e.Action != NESControllerButtonAction.Pressed) { return; }
+            switch (e.Button)
+            {
+                case NESControllerButton.Select:
+                    break;
+                case NESControllerButton.Start:
+                    break;
+                case NESControllerButton.B:
+                    break;
+                case NESControllerButton.A:
+                    break;
+                case NESControllerButton.Right:
+                    playerLocation.X++;
+                    break;
+                case NESControllerButton.Up:
+                    playerLocation.Y--;
+                    break;
+                case NESControllerButton.Down:
+                    playerLocation.Y++;
+                    break;
+                case NESControllerButton.Left:
+                    playerLocation.X--;
+                    break;
+                default:
+                    break;
+            }
+
+            if (playerLocation.X < 1) { playerLocation.X = 1; }
+            if (playerLocation.X >= mapSize.X - 1) { playerLocation.X = mapSize.X - 2; }
+            if (playerLocation.Y < 1) { playerLocation.Y = 1; }
+            if (playerLocation.Y >= mapSize.Y - 1) { playerLocation.Y = mapSize.Y - 2; }
+
+            if (playerLocation == food)
+            {
+                food = GetRandomPointOnBoard();
+            }
+            //DrawScreen();
+        }
+
+        private static Point GetRandomPointOnBoard()
+        {
+            return new Point(random.Next(1, mapSize.X - 2), random.Next(1, mapSize.Y - 2));
+        }
+
+        private static void TimerCallBackMethod(Object state)
+        {
+            playerChar = playerChar == 'O' ? 'o' : 'O';
+            DrawScreen();
+        }
+    }
+}
--- NESControllerLibrary/NESController.cs
+++ NESControllerLibrary/NESController.cs
@@ -8,35 +8,36 @@ public class NESController : NESControllerBase, IDisposable
     private byte[] _lastReadData = new byte[8] { 0, 0, 0, 0, 0, 0, 0, 0 };
     HIDDevice _device;
 
-    //You must change these (VID/PID) to your type of Dance Mat,
-    //in case you can't connect using this. Then your dance mat may be another brand.
+    //You must change these (VID/PID) to your type controller.
+    //You can find these in Device Manager > Human Interface Devices > Properties > Details > Property: Hardware ID
     //Look in Device manager, find your "HID-Compliant game controller" and look in
-    //properties > details > property: hardware ID
+    //in case you can't connect using this. Then your controller may be another brand.
+    //It may be possible for you to just change the VID/PID here in the code,
+    //it will depend on the data sent though.
+    
     private const int DEVICE_VENDOR_ID = 0x0810;
     private const int DEVICE_PRODUCT_ID = 0xE501;
 
     private static readonly Dictionary<NESControllerButton, Func<byte[], bool>> ButtonActivationRules = new() {
-        {NESControllerButton.Select, (byte[] input) => (input[3] & 16) > 0 },
-        {NESControllerButton.Start, (byte[] input) => (input[3] & 32) > 0 },
-        {NESControllerButton.A, (byte[] input) => (input[2] & 32) > 0 },
-        {NESControllerButton.B, (byte[] input) => (input[2] & 16) > 0 },
-        {NESControllerButton.Up, (byte[] input) => (input[1] & 127) == 0 },
-        {NESControllerButton.Down, (byte[] input) => (input[1] & 128) > 0 },
-        {NESControllerButton.Left, (byte[] input) => (input[0] &127) == 0 },
-        {NESControllerButton.Right, (byte[] input) => (input[0] & 128) > 0 },
+        {NESControllerButton.Select, input => (input[3] & 16) > 0 },
+        {NESControllerButton.Start, input => (input[3] & 32) > 0 },
+        {NESControllerButton.A, input => (input[2] & 32) > 0 },
+        {NESControllerButton.B, input => (input[2] & 16) > 0 },
+        {NESControllerButton.Up, input => (input[1] & 127) == 0 },
+        {NESControllerButton.Down, input => (input[1] & 128) > 0 },
+        {NESControllerButton.Left, input => (input[0] &127) == 0 },
+        {NESControllerButton.Right, input => (input[0] & 128) > 0 },
     };
 
     public NESController()
     {
-
-
         HIDDevice.interfaceDetails[] devices = HIDDevice.getConnectedDevices();
 
         //Select a device from the available devices (uses the Vendor ID and Product ID of the Dance Mat controller).
         var dev = devices.Where(dev => dev.VID == DEVICE_VENDOR_ID
             && dev.PID == DEVICE_PRODUCT_ID).FirstOrDefault();
 
-        if (dev.VID == 0) { throw new Exception("No Dance Mat controller detected. Do you need to change the driver to 'HID-Compliant game controller' in Device Manager?"); }
+        if (dev.VID == 0) { throw new Exception("No NEScontroller detected. Do you need to change the driver to 'HID-Compliant game controller' in Device Manager?"); }
 
         //register device, and set it up for publishing events when new data comes in
         _device = new HIDDevice(dev.devicePath, true);
@@ -47,12 +48,6 @@ public class NESController : NESControllerBase, IDisposable
 
     private void _device_dataReceived(byte[] messageInput)
     {
-        //Console.Write(message.Length + " : ");
-        //foreach (var byteData in message)
-        //{
-        //    Console.Write(byteData + ",");
-        //}
-        //Console.WriteLine();
         byte[] message = (byte[])messageInput.Clone();
         try
         {
@@ -69,15 +64,17 @@ public class NESController : NESControllerBase, IDisposable
                 var action = GetActionFromBitChange(
                     ButtonActivationRules[button](lastBitArray),
                     ButtonActivationRules[button](currentBitArray));
-                
+
+                #region debug info to console
                 //Console.WriteLine(Convert.ToString((long)BitConverter.ToUInt32(currentBitArray), 2).PadLeft(32, '0'));
                 //Console.WriteLine($"Previous:{Convert.ToString(lastBitArray[0], 2).PadLeft(8, '0')} {Convert.ToString(lastBitArray[1], 2).PadLeft(8, '0')} {Convert.ToString(lastBitArray[2], 2).PadLeft(8, '0')} {Convert.ToString(lastBitArray[3], 2).PadLeft(8, '0')}");
                 //Console.WriteLine($"Current :{Convert.ToString(currentBitArray[0], 2).PadLeft(8, '0')} {Convert.ToString(currentBitArray[1], 2).PadLeft(8, '0')} {Convert.ToString(currentBitArray[2], 2).PadLeft(8, '0')} {Convert.ToString(currentBitArray[3], 2).PadLeft(8, '0')}");
                 //Console.WriteLine("Action: " + action);
-                //Console.WriteLine();
+                //Console.WriteLine(); 
+                #endregion
+                
                 if (action != NESControllerButtonAction.Unchanged)
                 {
-                   
                     OnButtonStateChanged(button, action);
                 }
 
